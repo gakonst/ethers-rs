cbf046a07a64d861d5f920a39bed5d890a8bfe52
Matthias Seitz <matthias.seitz@outlook.de>
2021-03-11 20:47:20 +0100

    refactor: function parser

--- ethers-core/src/abi/human_readable.rs
+++ ethers-core/src/abi/human_readable.rs
@@ -47,16 +47,11 @@ pub fn parse(input: &[&str]) -> Result<Abi, ParseError> {
     Ok(abi)
 }
 
-/// Parses a solidity event declaration from `event <name> (args*) anonymous?`
-fn parse_event2(mut event: &str) -> Result<Event, ParseError> {
-    if !event.starts_with("event ") {
-        return Err(ParseError::ParseError(super::Error::InvalidData));
-    }
-    event = &event[6..];
-    let mut chars = event.chars();
+/// Parses an identifier like event or function name
+fn parse_identifier(input: &mut &str) -> Result<String, ParseError> {
+    let mut chars = input.trim_start().chars();
     let mut name = String::new();
-
-    let c = chars.next().unwrap();
+    let c = chars.next().ok_or(ParseError::ParseError(super::Error::InvalidData))?;
     if is_first_ident_char(c) {
         name.push(c);
         loop {
@@ -69,34 +64,41 @@ fn parse_event2(mut event: &str) -> Result<Event, ParseError> {
             }
         }
     }
+    *input = chars.as_str();
+    Ok(name)
+}
+
+/// Parses a solidity event declaration from `event <name> (args*) anonymous?`
+fn parse_event2(mut event: &str) -> Result<Event, ParseError> {
+    event = event.trim();
+    if !event.starts_with("event ") {
+        return Err(ParseError::ParseError(super::Error::InvalidData));
+    }
+    event = &event[6..];
+
+    let name = parse_identifier(&mut event)?;
     if name.is_empty() {
         return Err(ParseError::ParseError(super::Error::InvalidName(event.to_owned())));
     }
+
+    let mut chars = event.chars();
+
     loop {
         match chars.next() {
             None => return Err(ParseError::ParseError(super::Error::InvalidData)),
             Some('(') => {
                 event = chars.as_str().trim();
-                let (inputs, anonymous) = parse_event_args(event)?;
+                let mut anonymous = false;
+                if event.ends_with("anonymous") {
+                    anonymous = true;
+                    event = event[..event.len() - 9].trim_end();
+                }
+                let inputs = parse_event_args(event)?;
                 return Ok(Event {
                     name,
                     anonymous,
                     inputs,
-                })
-            }
-            Some('\\') => {
-                return match chars.next() {
-                    Some('(') => {
-                        event = chars.as_str().trim();
-                        let (inputs, anonymous) = parse_event_args(event)?;
-                        Ok(Event {
-                            name,
-                            anonymous,
-                            inputs,
-                        })
-                    }
-                    _ => Err(ParseError::ParseError(super::Error::InvalidData)),
-                }
+                });
             }
             Some(' ') | Some('\t') => {
                 continue;
@@ -108,21 +110,14 @@ fn parse_event2(mut event: &str) -> Result<Event, ParseError> {
     }
 }
 
-/// Returns the event parameters and whether the event was declared as anonymous
-fn parse_event_args(mut input: &str) -> Result<(Vec<EventParam>, bool), ParseError> {
-    let mut anonymous = false;
-    if input.ends_with("anonymous") {
-        anonymous = true;
-        input = input[..input.len()-9].trim_end();
-    }
-
+/// Returns the event parameters
+fn parse_event_args(mut input: &str) -> Result<Vec<EventParam>, ParseError> {
     input = input.trim().strip_suffix(')').ok_or(ParseError::ParseError(super::Error::InvalidData))?;
-    input = input.trim_end_matches('\\');
 
     let mut params = Vec::new();
 
     if input.is_empty() {
-        return  Ok((params, anonymous))
+        return Ok(params);
     }
 
     for arg in input.split(',') {
@@ -148,9 +143,67 @@ fn parse_event_args(mut input: &str) -> Result<(Vec<EventParam>, bool), ParseErr
             }
         )
     }
-    Ok((params, anonymous))
+    Ok(params)
 }
 
+
+fn parse_function2(mut input: &str) -> Result<Function, ParseError> {
+    input = input.trim();
+    if !input.starts_with("function ") {
+        return Err(ParseError::ParseError(super::Error::InvalidData));
+    }
+    input = &input[9..];
+    let name = parse_identifier(&mut input)?;
+    if name.is_empty() {
+        return Err(ParseError::ParseError(super::Error::InvalidName(input.to_owned())));
+    }
+
+    let mut iter = input.split(" returns");
+
+    let parens = iter.next().ok_or(ParseError::ParseError(super::Error::InvalidData))?.trim_end();
+
+    let mut parens_iter = parens.rsplitn(2, ')');
+    let mut modifiers = parens_iter.next();
+
+    let input_params = if let Some(args) = parens_iter.next() {
+        args
+    } else {
+        modifiers.take().ok_or(ParseError::ParseError(super::Error::InvalidData))?
+    }.trim_start().strip_prefix('(').ok_or(ParseError::ParseError(super::Error::InvalidData))?;
+
+
+    let inputs = input_params.split(',').filter(|s|!s.is_empty()).map(parse_param2).collect::<Result<Vec<_>,_>>()?;
+
+    let outputs = if let Some(params) = iter.next() {
+        let params = params.trim().strip_prefix('(').and_then(|s|s.strip_suffix(')')).ok_or(ParseError::ParseError(super::Error::InvalidData))?;
+        params.split(',').filter(|s|!s.is_empty()).map(parse_param2).collect::<Result<Vec<_>,_>>()?
+    } else {
+        Vec::new()
+    };
+
+    let state_mutability = if let Some(modifiers) = modifiers {
+        if modifiers.contains("pure") {
+            StateMutability::Pure
+        } else if modifiers.contains("view") {
+            StateMutability::View
+        } else if modifiers.contains("payable") {
+            StateMutability::Payable
+        } else {
+            StateMutability::NonPayable
+        }
+    } else {
+        StateMutability::NonPayable
+    };
+
+    #[allow(deprecated)]
+        Ok(Function {
+        name,
+        inputs,
+        outputs,
+        state_mutability,
+        constant: false,
+    })
+}
 
 fn parse2(input: &[&str]) -> Result<Abi, ParseError> {
     let mut abi = Abi {
@@ -162,22 +215,23 @@ fn parse2(input: &[&str]) -> Result<Abi, ParseError> {
     };
 
     for line in input {
-        let line = line.trim();
-        let (prefix, mut remainder) = line.split_once(' ').ok_or(ParseError::ParseError(super::Error::InvalidData))?;
-        match prefix {
-            "event" => {
-                let event = parse_event2(&mut remainder)?;
-                abi.events
-                    .entry(event.name.clone())
-                    .or_default()
-                    .push(event);
-            }
-            "function" => {}
-            "constructor" => {}
-            _ => return Err(ParseError::ParseError(super::Error::InvalidData))
+        let line = line.trim_start();
+        if line.starts_with("function") {
+            let function = parse_function2(&line)?;
+            abi.functions
+                .entry(function.name.clone())
+                .or_default()
+                .push(function);
+        } else if line.starts_with("event") {
+            let event = parse_event2(line)?;
+            abi.events
+                .entry(event.name.clone())
+                .or_default()
+                .push(event);
+        } else if line.starts_with("constructor") {} else {
+            return Err(ParseError::ParseError(super::Error::InvalidData));
         }
     }
-
 
     Ok(abi)
 }
@@ -286,6 +340,22 @@ fn parse_function(fn_string: &str) -> Result<Function, ParseError> {
     })
 }
 
+fn parse_param2(param: &str) -> Result<Param, ParseError> {
+    let mut iter = param.trim().rsplitn(3, is_whitespace);
+
+    let name = iter.next().ok_or(ParseError::ParseError(super::Error::InvalidData))?;
+
+    if let Some(ty) = iter.last() {
+        if name == "memory" || name == "calldata" {
+            Ok(Param { name: "".to_owned(), kind: Reader::read(ty)? })
+        } else {
+            Ok(Param { name: name.to_owned(), kind: Reader::read(ty)? })
+        }
+    } else {
+        Ok(Param { name: "".to_owned(), kind: Reader::read(name)? })
+    }
+}
+
 // address x
 fn parse_param(param: &str) -> Result<Param, ParseError> {
     let mut param = param
@@ -322,6 +392,19 @@ mod tests {
     use super::*;
 
     #[test]
+    fn parses_approve2() {
+        let fn_str = "function approve(address _spender, uint256 value) external returns(bool)";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.name, "approve");
+        assert_eq!(parsed.inputs[0].name, "_spender");
+        assert_eq!(parsed.inputs[0].kind, ParamType::Address, );
+        assert_eq!(parsed.inputs[1].name, "value");
+        assert_eq!(parsed.inputs[1].kind, ParamType::Uint(256), );
+        assert_eq!(parsed.outputs[0].name, "");
+        assert_eq!(parsed.outputs[0].kind, ParamType::Bool);
+    }
+
+    #[test]
     fn parses_approve() {
         let fn_str = "function approve(address _spender, uint256 value) external returns(bool)";
         let parsed = parse_function(fn_str).unwrap();
@@ -332,6 +415,20 @@ mod tests {
         assert_eq!(parsed.inputs[1].kind, ParamType::Uint(256), );
         assert_eq!(parsed.outputs[0].name, "");
         assert_eq!(parsed.outputs[0].kind, ParamType::Bool);
+    }
+
+    #[test]
+    fn parses_function_arguments_return2() {
+        let fn_str = "function foo(uint32[] memory x) external view returns (address)";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.name, "foo");
+        assert_eq!(parsed.inputs[0].name, "x");
+        assert_eq!(
+            parsed.inputs[0].kind,
+            ParamType::Array(Box::new(ParamType::Uint(32)))
+        );
+        assert_eq!(parsed.outputs[0].name, "");
+        assert_eq!(parsed.outputs[0].kind, ParamType::Address);
     }
 
     #[test]
@@ -346,6 +443,36 @@ mod tests {
         );
         assert_eq!(parsed.outputs[0].name, "");
         assert_eq!(parsed.outputs[0].kind, ParamType::Address);
+    }
+
+    #[test]
+    fn parses_function_empty2() {
+        let fn_str = "function foo()";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.name, "foo");
+        assert!(parsed.inputs.is_empty());
+        assert!(parsed.outputs.is_empty());
+    }
+
+    #[test]
+    fn parses_function_payable() {
+        let fn_str = "function foo() public payable";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.state_mutability, StateMutability::Payable);
+    }
+
+    #[test]
+    fn parses_function_view() {
+        let fn_str = "function foo() external view";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.state_mutability, StateMutability::View);
+    }
+
+    #[test]
+    fn parses_function_pure() {
+        let fn_str = "function foo()  pure";
+        let parsed = parse_function2(fn_str).unwrap();
+        assert_eq!(parsed.state_mutability, StateMutability::Pure);
     }
 
     #[test]
@@ -456,6 +583,22 @@ mod tests {
                 indexed: false,
             }
         );
+    }
+
+    #[test]
+    fn can_parse_functions2() {
+        [
+            "function foo(uint256[] memory x) external view returns (address)",
+            "function bar(uint256[] memory x) returns (address)",
+            "function bar(uint256[] memory x, uint32 y) returns (address, uint256)",
+            "function foo(address[] memory, bytes memory) returns (bytes memory)",
+            "function bar(uint256[] memory x)",
+            "function bar()",
+        ]
+            .iter()
+            .for_each(|x| {
+                parse_function2(x).unwrap();
+            });
     }
 
     #[test]
