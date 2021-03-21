fe7dc0718e04fe1eb1a23af888ace7d71972280c
Matthias Seitz <matthias.seitz@outlook.de>
2021-03-21 22:04:42 +0100

    chore: update examples with new setup functions

--- ethers/examples/contract_with_abi.rs
+++ ethers/examples/contract_with_abi.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use ethers::{
     prelude::*,
-    utils::{Ganache, Solc},
+    utils::{compile_and_launch_ganache, Ganache, Solc},
 };
 use std::{convert::TryFrom, sync::Arc, time::Duration};
 
@@ -15,55 +15,54 @@ abigen!(
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // 1. compile the contract (note this requires that you are inside the `ethers/examples` directory)
-    let compiled = Solc::new("**/contract.sol").build()?;
+    // 1. compile the contract (note this requires that you are inside the `ethers/examples` directory) and launch ganache
+    let (compiled, ganache) =
+        compile_and_launch_ganache(Solc::new("**/contract.sol"), Ganache::new()).await?;
+
     let contract = compiled
         .get("SimpleStorage")
         .expect("could not find contract");
     dbg!("OK");
 
-    // 2. launch ganache
-    let ganache = Ganache::new().spawn();
-
-    // 3. instantiate our wallet
+    // 2. instantiate our wallet
     let wallet: LocalWallet = ganache.keys()[0].clone().into();
 
-    // 4. connect to the network
+    // 3. connect to the network
     let provider =
         Provider::<Http>::try_from(ganache.endpoint())?.interval(Duration::from_millis(10u64));
 
-    // 5. instantiate the client with the wallet
+    // 4. instantiate the client with the wallet
     let client = SignerMiddleware::new(provider, wallet);
     let client = Arc::new(client);
 
-    // 6. create a factory which will be used to deploy instances of the contract
+    // 5. create a factory which will be used to deploy instances of the contract
     let factory = ContractFactory::new(
         contract.abi.clone(),
         contract.bytecode.clone(),
         client.clone(),
     );
 
-    // 7. deploy it with the constructor arguments
+    // 6. deploy it with the constructor arguments
     let contract = factory.deploy("initial value".to_string())?.send().await?;
 
-    // 8. get the contract's address
+    // 7. get the contract's address
     let addr = contract.address();
 
-    // 9. instantiate the contract
+    // 8. instantiate the contract
     let contract = SimpleContract::new(addr, client.clone());
 
-    // 10. call the `setValue` method
+    // 9. call the `setValue` method
     // (first `await` returns a PendingTransaction, second one waits for it to be mined)
     let _receipt = contract.set_value("hi".to_owned()).send().await?.await?;
 
-    // 11. get all events
+    // 10. get all events
     let logs = contract
         .value_changed_filter()
         .from_block(0u64)
         .query()
         .await?;
 
-    // 12. get the new value
+    // 11. get the new value
     let value = contract.get_value().call().await?;
 
     println!("Value: {}. Logs: {}", value, serde_json::to_string(&logs)?);
