37ea4af140c3efbb27ace8120231b20e7303ada5
Jonathan LEI <xJonathan@outlook.com>
2021-06-07 14:17:15 +0000

    Fix tests failing on solc 0.8.0 or above

--- ethers-middleware/tests/solidity-contracts/DSProxy.sol
+++ ethers-middleware/tests/solidity-contracts/DSProxy.sol
@@ -48,7 +48,7 @@ contract DSAuth is DSAuthEvents {
             return true;
         } else if (src == owner) {
             return true;
-        } else if (authority == DSAuthority(0)) {
+        } else if (authority == DSAuthority(address(uint160(0)))) {
             return false;
         } else {
             return authority.canCall(src, address(this), sig);
