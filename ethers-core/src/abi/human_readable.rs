6d2ac415f197f81e016f99f661711f7f57125197
Matthias Seitz <matthias.seitz@outlook.de>
2021-10-01 22:06:05 +0200

    make clippy happy

--- ethers-core/src/abi/human_readable.rs
+++ ethers-core/src/abi/human_readable.rs
@@ -289,16 +289,8 @@ impl AbiParser {
             "" => None,
             input_params_args => Some(input_params_args),
         };
-        let modifiers = match input_args_modifiers_iter
-            .next()
-            .ok_or_else(|| format_err!("Expected input args parentheses at `{}`", s))?
-        {
-            "" => None,
-            modifiers => Some(modifiers),
-        };
 
         let inputs = if let Some(params) = input_args {
-            
             self
                 .parse_params(params)?
                 .into_iter()
