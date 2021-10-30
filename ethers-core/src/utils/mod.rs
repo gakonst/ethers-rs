911eaaec3876f274e5a4cd990d21a7e7ab76eeb7
Georgios Konstantopoulos <me@gakonst.com>
2021-10-30 21:39:11 +0200

    chore: remove solc from ethers-core

--- ethers-core/src/utils/mod.rs
+++ ethers-core/src/utils/mod.rs
@@ -10,13 +10,6 @@ mod geth;
 #[cfg(not(target_arch = "wasm32"))]
 pub use geth::{Geth, GethInstance};
 
-/// Solidity compiler bindings
-#[cfg(not(target_arch = "wasm32"))]
-pub mod solc;
-
-#[cfg(not(target_arch = "wasm32"))]
-pub use solc::{CompiledContract, Solc};
-
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(feature = "setup")]
 mod setup;
