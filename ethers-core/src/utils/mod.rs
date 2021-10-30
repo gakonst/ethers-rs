5f9388aecf8331b6acb72c767308f0549be4ac7a
Georgios Konstantopoulos <me@gakonst.com>
2021-10-30 22:50:04 +0200

    chore: remove concurrent setup code from ethers-core

--- ethers-core/src/utils/mod.rs
+++ ethers-core/src/utils/mod.rs
@@ -10,13 +10,6 @@ mod geth;
 #[cfg(not(target_arch = "wasm32"))]
 pub use geth::{Geth, GethInstance};
 
-#[cfg(not(target_arch = "wasm32"))]
-#[cfg(feature = "setup")]
-mod setup;
-#[cfg(not(target_arch = "wasm32"))]
-#[cfg(feature = "setup")]
-pub use setup::*;
-
 mod hash;
 pub use hash::{hash_message, id, keccak256, serialize};
 
