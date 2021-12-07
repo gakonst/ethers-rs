47c081115154817d0d0f4260476a35cdb69b65bc
Matthias Seitz <matthias.seitz@outlook.de>
2021-12-07 04:54:32 +0100

    revert changes

--- ethers-solc/src/artifacts.rs
+++ ethers-solc/src/artifacts.rs
@@ -840,13 +840,19 @@ impl BytecodeObject {
     /// Tries to resolve the unlinked string object a valid bytecode object in place
     ///
     /// Returns the string if it is a valid
-    pub fn resolve(&mut self) -> Option<&Bytes> {
+    pub fn resolve(&mut self) -> Option<LinkedByteCode> {
+        let mut resolved = None;
+
         if let BytecodeObject::Unlinked(unlinked) = self {
             if let Ok(linked) = hex::decode(unlinked) {
-                *self = BytecodeObject::Bytecode(linked.into());
+                resolved = Some(BytecodeObject::Bytecode(linked.into()));
             }
         }
-        self.as_bytes()
+        if let Some(resolved) = resolved {
+            std::mem::replace(self, resolved).into_unlinked().map(LinkedByteCode)
+        } else {
+            None
+        }
     }
 
     /// Link using the fully qualified name of a library
@@ -907,6 +913,16 @@ impl AsRef<[u8]> for BytecodeObject {
             BytecodeObject::Bytecode(code) => code.as_ref(),
             BytecodeObject::Unlinked(code) => code.as_bytes(),
         }
+    }
+}
+
+/// A fully linked byte code that represents a valid hex string
+#[derive(Debug, Clone)]
+pub struct LinkedByteCode(String);
+
+impl From<LinkedByteCode> for Bytes {
+    fn from(inner: LinkedByteCode) -> Self {
+        hex::decode(&inner.0).expect("Is valid hex").into()
     }
 }
 
