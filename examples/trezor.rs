a20b09f750091a695b2fc539592cc1d8d24e2af5
joshieDo <ranriver@protonmail.com>
2021-12-10 17:07:16 +0200

    no ens

--- examples/trezor.rs
+++ examples/trezor.rs
@@ -13,7 +13,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let trezor = Trezor::new(TrezorHDPath::TrezorLive(0), 1).await?;
     let client = SignerMiddleware::new(provider, trezor);
 
-    // Create and broadcast a transaction (ENS enabled!)
+    // Create and broadcast a transaction (ENS disabled!)
     // (this will require confirming the tx on the device)
     let tx = TransactionRequest::new()
         .to("0x99E2B13A8Ea8b00C68FA017ee250E98e870D8241")
