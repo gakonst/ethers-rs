2af1b28adff9d07343066d728e7926dfbbe11e7b
Georgios Konstantopoulos <me@gakonst.com>
2021-12-21 00:05:37 +0200

    chore: cargo fmt

--- ethers-core/src/utils/mod.rs
+++ ethers-core/src/utils/mod.rs
@@ -26,8 +26,8 @@ pub use rlp;
 pub use hex;
 
 use crate::types::{Address, Bytes, U256};
-use ethabi::ethereum_types::FromDecStrErr;
 use elliptic_curve::sec1::ToEncodedPoint;
+use ethabi::ethereum_types::FromDecStrErr;
 use k256::{ecdsa::SigningKey, PublicKey as K256PublicKey};
 use std::{convert::TryInto, ops::Neg};
 use thiserror::Error;
--- ethers-core/src/utils/moonbeam.rs
+++ ethers-core/src/utils/moonbeam.rs
@@ -56,39 +56,57 @@ impl Default for MoonbeamDev {
             keys: BTreeMap::from([
                 (
                     "Alith",
-                    to_secret_key("5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133"),
+                    to_secret_key(
+                        "5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133",
+                    ),
                 ),
                 (
                     "Baltathar",
-                    to_secret_key("8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b"),
+                    to_secret_key(
+                        "8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b",
+                    ),
                 ),
                 (
                     "Charleth",
-                    to_secret_key("0b6e18cafb6ed99687ec547bd28139cafdd2bffe70e6b688025de6b445aa5c5b"),
+                    to_secret_key(
+                        "0b6e18cafb6ed99687ec547bd28139cafdd2bffe70e6b688025de6b445aa5c5b",
+                    ),
                 ),
                 (
                     "Dorothy",
-                    to_secret_key("39539ab1876910bbf3a223d84a29e28f1cb4e2e456503e7e91ed39b2e7223d68"),
+                    to_secret_key(
+                        "39539ab1876910bbf3a223d84a29e28f1cb4e2e456503e7e91ed39b2e7223d68",
+                    ),
                 ),
                 (
                     "Faith",
-                    to_secret_key("b9d2ea9a615f3165812e8d44de0d24da9bbd164b65c4f0573e1ce2c8dbd9c8df"),
+                    to_secret_key(
+                        "b9d2ea9a615f3165812e8d44de0d24da9bbd164b65c4f0573e1ce2c8dbd9c8df",
+                    ),
                 ),
                 (
                     "Goliath",
-                    to_secret_key("96b8a38e12e1a31dee1eab2fffdf9d9990045f5b37e44d8cc27766ef294acf18"),
+                    to_secret_key(
+                        "96b8a38e12e1a31dee1eab2fffdf9d9990045f5b37e44d8cc27766ef294acf18",
+                    ),
                 ),
                 (
                     "Heath",
-                    to_secret_key("0d6dcaaef49272a5411896be8ad16c01c35d6f8c18873387b71fbc734759b0ab"),
+                    to_secret_key(
+                        "0d6dcaaef49272a5411896be8ad16c01c35d6f8c18873387b71fbc734759b0ab",
+                    ),
                 ),
                 (
                     "Ida",
-                    to_secret_key("4c42532034540267bf568198ccec4cb822a025da542861fcb146a5fab6433ff8"),
+                    to_secret_key(
+                        "4c42532034540267bf568198ccec4cb822a025da542861fcb146a5fab6433ff8",
+                    ),
                 ),
                 (
                     "Judith",
-                    to_secret_key("94c49300a58d576011096bcb006aa06f5a91b34b4383891e8029c21dc39fbb8b"),
+                    to_secret_key(
+                        "94c49300a58d576011096bcb006aa06f5a91b34b4383891e8029c21dc39fbb8b",
+                    ),
                 ),
             ]),
         }
