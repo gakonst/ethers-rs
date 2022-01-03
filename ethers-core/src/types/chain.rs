baddf31727a0786da45ce00de7e5b2eb590dbf58
Odysseas Lamtzidis <odyslam@gmail.com>
2022-01-04 00:55:26 +0300

    feat: add FromStr impl for Chain

--- ethers-core/src/types/chain.rs
+++ ethers-core/src/types/chain.rs
@@ -1,7 +1,16 @@
 use std::fmt;
 
 use crate::types::U256;
+use std::str::FromStr;
 
+#[derive(Debug, Clone)]
+pub struct ParseChainError;
+
+impl fmt::Display for ParseChainError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Failed to parse: {:?}", self)
+    }
+}
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Chain {
     Mainnet,
@@ -60,5 +69,30 @@ impl From<Chain> for U256 {
 impl From<Chain> for u64 {
     fn from(chain: Chain) -> Self {
         u32::from(chain).into()
+    }
+}
+
+impl FromStr for Chain {
+    type Err = ParseChainError;
+    fn from_str(chain: &str) -> Result<Self, Self::Err> {
+        Ok(match chain {
+            "mainnet" => Chain::Mainnet,
+            "ropsten" => Chain::Ropsten,
+            "rinkeby" => Chain::Rinkeby,
+            "goerli" => Chain::Goerli,
+            "kovan" => Chain::Kovan,
+            "xdai" => Chain::XDai,
+            "polygon" => Chain::Polygon,
+            "polygonMumbai" => Chain::PolygonMumbai,
+            "avalanche" => Chain::Avalanche,
+            "avalancheFuji" => Chain::AvalancheFuji,
+            "sepolia" => Chain::Sepolia,
+            "moonbeam" => Chain::Moonbeam,
+            "moonbeamDev" => Chain::MoonbeamDev,
+            "moonriver" => Chain::Moonriver,
+            "optimism" => Chain::Optimism,
+            "optimismKovan" => Chain::OptimismKovan,
+            _ => Chain::Mainnet,
+        })
     }
 }
