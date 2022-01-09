dd6dea020fb970f812fabee1ffba677bae70decf
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-01-09 21:53:56 +0800

    chore: docs for ethers-addressbook crate

--- ethers-addressbook/src/lib.rs
+++ ethers-addressbook/src/lib.rs
@@ -9,17 +9,22 @@ const CONTRACTS_JSON: &str = include_str!("./contracts/contracts.json");
 static ADDRESSBOOK: Lazy<HashMap<String, Contract>> =
     Lazy::new(|| serde_json::from_str(CONTRACTS_JSON).unwrap());
 
+/// Wrapper around a hash map that maps a [chain](https://github.com/gakonst/ethers-rs/blob/master/ethers-core/src/types/chain.rs) to the contract's deployed address on that chain.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Contract {
     addresses: HashMap<Chain, Address>,
 }
 
 impl Contract {
+    /// Returns the address of the contract on the specified chain. If the contract's address is
+    /// not found in the addressbook, the getter returns None.
     pub fn address(&self, chain: Chain) -> Option<Address> {
         self.addresses.get(&chain).cloned()
     }
 }
 
+/// Fetch the addressbook for a contract by its name. If the contract name is not a part of
+/// [ethers-addressbook](https://github.com/gakonst/ethers-rs/tree/master/ethers-addressbook) we return None.
 pub fn contract<S: Into<String>>(name: S) -> Option<Contract> {
     ADDRESSBOOK.get(&name.into()).cloned()
 }
