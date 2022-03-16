864944137bd53b409bab407950fb145f818dcc8e
Matthias Seitz <matthias.seitz@outlook.de>
2022-03-16 12:21:51 +0100

    ci: add examples build step and set chmod +x examples.sh

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,7 +1,7 @@
+set -e
 # shellcheck shell=bash
 # run all examples
 for file in examples/*.rs; do
   name="$(echo "$file" | cut -f 1 -d '.')"
-  echo "running $name"
   cargo r -p ethers --example "$(basename "$name")"
 done
