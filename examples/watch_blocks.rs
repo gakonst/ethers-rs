8669a27123c01a175109fc594c7d959e119523cc
nikkolasg <nikkolasg@gmail.com>
2022-03-16 12:40:46 +0000

    better watch example

--- examples/watch_blocks.rs
+++ examples/watch_blocks.rs
@@ -5,10 +5,11 @@ use std::time::Duration;
 async fn main() -> eyre::Result<()> {
     let ganache = Ganache::new().block_time(1u64).spawn();
     let ws = Ws::connect(ganache.ws_endpoint()).await?;
-    let provider = Provider::new(ws).interval(Duration::from_millis(2000));
+    let provider = Provider::new(ws).interval(Duration::from_millis(500));
     let mut stream = provider.watch_blocks().await?.take(5);
     while let Some(block) = stream.next().await {
-        dbg!(block);
+        let block = provider.get_block(block).await?.unwrap();
+        println!("Ts: {:?}, block number: {} -> {:?}",block.timestamp,block.number.unwrap(),block.hash.unwrap());
     }
 
     Ok(())
