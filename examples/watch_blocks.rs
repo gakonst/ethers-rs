c9ef5769e2f614574050cc17f28bff6b452815b9
Nicolas Gailly <nikkolasg@users.noreply.github.com>
2022-03-18 05:08:15 +0000

    Examples/watch_block: show how to retrieve block metadata (#1058)
    * better watch example
    
    * put back normal interval

--- examples/watch_blocks.rs
+++ examples/watch_blocks.rs
@@ -8,7 +8,8 @@ async fn main() -> eyre::Result<()> {
     let provider = Provider::new(ws).interval(Duration::from_millis(2000));
     let mut stream = provider.watch_blocks().await?.take(5);
     while let Some(block) = stream.next().await {
-        dbg!(block);
+        let block = provider.get_block(block).await?.unwrap();
+        println!("Ts: {:?}, block number: {} -> {:?}",block.timestamp,block.number.unwrap(),block.hash.unwrap());
     }
 
     Ok(())
