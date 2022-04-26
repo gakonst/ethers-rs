9cbb32dd690ec05c621cc9720fb710572c4c9d6a
Matthias Seitz <matthias.seitz@outlook.de>
2022-04-26 10:26:15 +0200

    chore(solc): remove async feature requirement

--- ethers-solc/src/compile/mod.rs
+++ ethers-solc/src/compile/mod.rs
@@ -224,7 +224,7 @@ impl Solc {
 
     /// Returns the list of all versions that are available to download and marking those which are
     /// already installed.
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     pub fn all_versions() -> Vec<SolcVersion> {
         let mut all_versions = Self::installed_versions();
         let mut uniques = all_versions
@@ -285,7 +285,7 @@ impl Solc {
     /// to build it, and returns it.
     ///
     /// If the required compiler version is not installed, it also proceeds to install it.
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     pub fn detect_version(source: &Source) -> Result<Version> {
         // detects the required solc version
         let sol_version = Self::source_version_req(source)?;
@@ -296,7 +296,7 @@ impl Solc {
     /// used to build it, and returns it.
     ///
     /// If the required compiler version is not installed, it also proceeds to install it.
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     pub fn ensure_installed(sol_version: &VersionReq) -> Result<Version> {
         #[cfg(any(test, feature = "tests"))]
         let _lock = take_solc_installer_lock();
@@ -371,7 +371,7 @@ impl Solc {
     }
 
     /// Blocking version of `Self::install`
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     pub fn blocking_install(version: &Version) -> std::result::Result<Self, svm::SolcVmError> {
         tracing::trace!("blocking installing solc version \"{}\"", version);
         crate::report::solc_installation_start(version);
@@ -389,7 +389,7 @@ impl Solc {
 
     /// Verify that the checksum for this version of solc is correct. We check against the SHA256
     /// checksum from the build information published by binaries.soliditylang
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     pub fn verify_checksum(&self) -> Result<()> {
         let version = self.version_short()?;
         let mut version_path = svm::version_path(version.to_string().as_str());
@@ -733,7 +733,7 @@ mod tests {
 
     #[test]
     // This test might be a bit hard to maintain
-    #[cfg(all(feature = "svm-solc", feature = "async"))]
+    #[cfg(all(feature = "svm-solc"))]
     fn test_detect_version() {
         for (pragma, expected) in [
             // pinned
