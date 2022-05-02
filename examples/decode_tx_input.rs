ab80665a01d9eb89117c8af9577d4cc068c79956
Astronaut Sleuth <98103193+AstronautSleuth@users.noreply.github.com>
2022-05-03 02:53:20 +0800

    fixed typo when determining to token address (#1208)

--- examples/decode_tx_input.rs
+++ examples/decode_tx_input.rs
@@ -14,11 +14,12 @@ fn main() -> Result<()> {
     let tx_input = "0x38ed173900000000000000000000000000000000000000000001a717cc0a3e4f84c00000000000000000000000000000000000000000000000000000000000000283568400000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000201f129111c60401630932d9f9811bd5b5fff34e000000000000000000000000000000000000000000000000000000006227723d000000000000000000000000000000000000000000000000000000000000000200000000000000000000000095ad61b0a150d79219dcf64e1e6cc01f0b64c4ce000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7";
     let calldata: Bytes = tx_input.parse().unwrap();
     let decoded = SwapExactTokensForTokensCall::decode(&calldata)?;
-
-    let from = decoded.path.into_iter().next().unwrap();
+    let mut path = decoded.path.into_iter();
+    let from = path.next().unwrap();
+    let to = path.next().unwrap();
     println!(
         "Swapped {} of token {} for {} of token {}",
-        decoded.amount_in, from, decoded.amount_out_min, decoded.to
+        decoded.amount_in, from, decoded.amount_out_min, to
     );
 
     Ok(())
