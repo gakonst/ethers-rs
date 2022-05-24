9a26252a47e0031454643ddd309c31930b222388
Meet Mangukiya <meet@flamy.dev>
2022-05-24 14:02:54 +0000

    remove backoff abstractions

--- ethers-providers/src/transports/retry.rs
+++ ethers-providers/src/transports/retry.rs
@@ -7,68 +7,55 @@ use crate::{provider::ProviderError, JsonRpcClient};
 use std::{
     clone::Clone,
     fmt::Debug,
-    sync::{
-        atomic::{AtomicU32, Ordering},
-        Mutex,
-    },
+    sync::atomic::{AtomicU64, Ordering},
     time::Duration,
 };
 
 use async_trait::async_trait;
-use backoff::ExponentialBackoff as BackoffExponentialBackoff;
-#[cfg(not(target_arch = "wasm32"))]
-use futures_timer::Delay;
 use serde::{de::DeserializeOwned, Serialize};
 use thiserror::Error;
-#[cfg(target_arch = "wasm32")]
-use wasm_timer::Delay;
 
 /// [RetryPolicy] defines logic for which [JsonRpcClient::Error] instances should
 /// the client retry the request and try to recover from.
-pub trait RetryPolicy<E> {
+pub trait RetryPolicy<E>: Send + Sync + Debug {
     fn should_retry(&self, error: &E) -> bool;
-}
-
-/// [Backoff] defines the logic for determining the [Duration] for which
-/// the client must backoff before retrying the request.
-pub trait Backoff {
-    ///
-    fn next_backoff(&mut self, total_enqueued_requests: u32, retry_number: u64)
-        -> Option<Duration>;
 }
 
 /// [RetryClient] presents as a wrapper around [JsonRpcClient] that will retry
 /// requests based on [Backoff] strategy and filtering based on [RetryPolicy].
 #[derive(Debug)]
-pub struct RetryClient<T, U, B>
+pub struct RetryClient<T>
 where
     T: JsonRpcClient,
     T::Error: Sync + Send + 'static,
-    U: RetryPolicy<T::Error> + Send + Sync + Debug,
-    B: Backoff + Send + Sync,
 {
     inner: T,
-    requests_enqueued: AtomicU32,
-    policy: U,
-    backoff: Mutex<B>,
+    requests_enqueued: AtomicU64,
+    policy: Box<dyn RetryPolicy<T::Error>>,
+    max_retry: u64,
+    initial_backoff: u64,
 }
 
-impl<T, U, B> RetryClient<T, U, B>
+impl<T> RetryClient<T>
 where
     T: JsonRpcClient,
     T::Error: Sync + Send + 'static,
-    U: RetryPolicy<T::Error> + Send + Sync + Debug,
-    B: Backoff + Send + Sync,
 {
     /// Example:
     ///
     /// ```no_run
     /// let http = Http::new(Url::parse("http://localhost:8545").unwrap());
     /// let delay = Duration::new(10, 0);
-    /// let client = RetryClient::new(http, HttpRateLimitRetryPolicy::new(), DelayedQueuedBackoff::new(delay));
+    /// let client = RetryClient::new(http, HttpRateLimitRetryPolicy::new(), 10, 1);
     /// ```
-    pub fn new(inner: T, policy: U, backoff: B) -> Self {
-        Self { inner, requests_enqueued: AtomicU32::new(0), policy, backoff: Mutex::new(backoff) }
+    pub fn new(
+        inner: T,
+        policy: Box<dyn RetryPolicy<T::Error>>,
+        max_retry: u64,
+        // in seconds
+        initial_backoff: u64,
+    ) -> Self {
+        Self { inner, requests_enqueued: AtomicU64::new(0), policy, max_retry, initial_backoff }
     }
 }
 
@@ -109,14 +96,11 @@ where
     }
 }
 
-#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-impl<T, U, B> JsonRpcClient for RetryClient<T, U, B>
+impl<T> JsonRpcClient for RetryClient<T>
 where
     T: JsonRpcClient + 'static,
     T::Error: Sync + Send + 'static,
-    U: RetryPolicy<T::Error> + Send + Sync + Debug,
-    B: Backoff + Send + Sync + Debug + Clone,
 {
     type Error = RetryClientError<T>;
 
@@ -130,7 +114,7 @@ where
         let params =
             serde_json::to_value(params).map_err(|err| RetryClientError::SerdeJson(err))?;
 
-        let mut retry_number = 0;
+        let mut retry_number: u64 = 0;
 
         loop {
             let err;
@@ -147,18 +131,15 @@ where
             }
 
             retry_number += 1;
+            if retry_number > self.max_retry {
+                return Err(RetryClientError::TimeoutError)
+            }
+
             let should_retry = self.policy.should_retry(&err);
             if should_retry {
-                let next_backoff = self
-                    .backoff
-                    .lock()
-                    .unwrap()
-                    .next_backoff(self.requests_enqueued.load(Ordering::SeqCst), retry_number);
-                if next_backoff.is_none() {
-                    return Err(RetryClientError::TimeoutError)
-                }
-                let next_backoff = next_backoff.unwrap();
-                Delay::new(next_backoff).await;
+                let current_queued_requests = self.requests_enqueued.load(Ordering::SeqCst);
+                let next_backoff = self.initial_backoff * (retry_number + current_queued_requests);
+                tokio::time::sleep(Duration::new(next_backoff, 0)).await;
                 continue
             } else {
                 return Err(RetryClientError::ProviderError(err))
@@ -180,78 +161,5 @@ impl RetryPolicy<ClientError> for HttpRateLimitRetryPolicy {
             }
             _ => false,
         }
-    }
-}
-
-/// Implements [Backoff] strategy that imposes 1 request per [`delay`](Duration)
-/// until max retries have reached.
-pub struct DelayedQueuedBackoff {
-    delay: Duration,
-    max_retry: u64,
-}
-
-impl DelayedQueuedBackoff {
-    pub fn new(delay: Duration, max_retry: u64) -> Self {
-        Self { delay, max_retry }
-    }
-}
-
-impl Backoff for DelayedQueuedBackoff {
-    fn next_backoff(
-        &mut self,
-        total_enqueued_requests: u32,
-        retry_number: u64,
-    ) -> Option<Duration> {
-        if retry_number > self.max_retry {
-            None
-        } else {
-            let secs = self.delay.as_secs();
-            let nsecs = self.delay.subsec_nanos();
-            Some(Duration::new(
-                secs * total_enqueued_requests as u64,
-                nsecs * total_enqueued_requests,
-            ))
-        }
-    }
-}
-
-/// [Backoff] strategy that uses [backoff::ExponentialBackoff] internally.
-pub struct ExponentialBackoff {
-    internal: BackoffExponentialBackoff,
-}
-
-impl ExponentialBackoff {
-    pub fn new() -> Self {
-        Self { internal: BackoffExponentialBackoff::default() }
-    }
-}
-
-impl std::default::Default for ExponentialBackoff {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl std::ops::Deref for ExponentialBackoff {
-    type Target = BackoffExponentialBackoff;
-
-    fn deref(&self) -> &Self::Target {
-        &self.internal
-    }
-}
-
-impl std::ops::DerefMut for ExponentialBackoff {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.internal
-    }
-}
-
-impl Backoff for ExponentialBackoff {
-    fn next_backoff(
-        &mut self,
-        _total_enqueued_requests: u32,
-        _retry_number: u64,
-    ) -> Option<Duration> {
-        backoff::backoff::Backoff::next_backoff(&mut self.internal)
     }
 }
