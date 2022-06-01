6dcbfafb06c4f05a0d024227c2d57e12ba358be5
Matthias Seitz <matthias.seitz@outlook.de>
2022-06-01 12:56:10 +0200

    fix: update examples script

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,7 +1,16 @@
 set -e
 # shellcheck shell=bash
+
+# examples that we can't run because they require some additional infra, docker for example
+ignored=["examples/moonbeam_with_abi.rs"]
+
 # run all examples
 for file in examples/*.rs; do
+  if [[ "${ignored[*]}" =~ $file ]]; then
+      echo "skipping: $file"
+      continue
+  fi
+  echo "running: $file"
   name="$(echo "$file" | cut -f 1 -d '.')"
-  cargo r -p ethers --example "$(basename "$name")"
+  cargo r -p ethers --example "$(basename "$name")" --features = "ipc,ledger,trezor,yubi"
 done
