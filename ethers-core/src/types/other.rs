8b1e73077a3a6a3452c46fe40b5b0b33945580c4
Matthias Seitz <matthias.seitz@outlook.de>
2022-06-28 21:33:14 +0200

    fix: make docs compile on stable

--- ethers-core/src/types/other.rs
+++ ethers-core/src/types/other.rs
@@ -87,7 +87,7 @@ impl OtherFields {
     /// ```
     /// # use ethers_core::types::{OtherFields, U64};
     /// fn d(mut other: OtherFields) {
-    ///  let (key, l1_block_number_result) = other.remove_entry_deserialized::<U64>("l1BlockNumber").unwrap();
+    ///  let (key, l1_block_number_result) : (_, serde_json::Result<U64>) = other.remove_entry_deserialized("l1BlockNumber").unwrap();
     /// let l1_block_number = l1_block_number_result.unwrap();
     /// assert!(!other.contains_key("l1BlockNumber"));
     /// # }
