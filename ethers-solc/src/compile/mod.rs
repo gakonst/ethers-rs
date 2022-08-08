86ce1018123ea2d92050bb810aedff48e487583a
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-08 20:14:39 +0200

    chore: update max solc version 0.8.16

--- ethers-solc/src/compile/mod.rs
+++ ethers-solc/src/compile/mod.rs
@@ -823,7 +823,7 @@ mod tests {
             // update this test whenever there's a new sol
             // version. that's ok! good reminder to check the
             // patch notes.
-            (">=0.5.0", "0.8.15"),
+            (">=0.5.0", "0.8.16"),
             // range
             (">=0.4.0 <0.5.0", "0.4.26"),
         ]
