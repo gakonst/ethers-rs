be6bd7c1170acce7cef6d06458df9cf0bd342d06
Drawn Wren <drawnwren@protonmail.com>
2022-08-10 16:01:51 -0400

    feat(TraceType+Deserialize): added Deserialize to TraceType

--- ethers-core/src/types/trace/mod.rs
+++ ethers-core/src/types/trace/mod.rs
@@ -11,7 +11,7 @@ pub use filter::*;
 mod geth;
 pub use geth::*;
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 /// Description of the type of trace to make
 pub enum TraceType {
     /// Transaction Trace
