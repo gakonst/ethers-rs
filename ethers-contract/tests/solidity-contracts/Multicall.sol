f8cd431b3a581ce9674780657aee59d10f48cf75
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-08-10 22:36:09 +0000

    fix: solidity minimum version for Multicall.sol

--- ethers-contract/tests/solidity-contracts/Multicall.sol
+++ ethers-contract/tests/solidity-contracts/Multicall.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.12;
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
 
 /// @title Multicall3
 /// @notice Aggregate results from multiple function calls
