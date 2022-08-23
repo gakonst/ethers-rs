9f09ca4c27bb68aad3c88ccb77b8f33aceea4130
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-23 15:57:37 +0200

    fix: dont skip null to field

--- ethers-core/src/types/transaction/response.rs
+++ ethers-core/src/types/transaction/response.rs
@@ -40,7 +40,7 @@ pub struct Transaction {
     pub from: Address,
 
     /// Recipient (None when contract creation)
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
     pub to: Option<Address>,
 
     /// Transferred value
