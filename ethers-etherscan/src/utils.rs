62beb6cf53caf9c98f1ce0c793d9992d778cee1f
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-09-29 17:07:50 +0200

    feat(etherscan): parse SourceCode field (#1747)
    * wip
    
    * feat(etherscan): parse SourceCode field
    
    * feat: add project builder method
    
    * chore: dependencies
    
    * docs
    
    * refactor: impls
    
    * refactor: create verify submodule
    
    * refactor: use untagged enum
    
    * test: add more assertions
    
    * docs
    
    * feat: add more utility methods
    
    * feat: deserialize ABI and improve SourceCode deserialization
    
    * fix: lookup_compiler_version
    
    * refactor: source tree methods
    
    * docs
    
    * chore: add solc feature
    
    * fix: test
    
    * chore: use only optional dependency
    
    * chore: re-add dev-dependency

--- ethers-etherscan/src/utils.rs
+++ ethers-etherscan/src/utils.rs
@@ -1,29 +1,70 @@
+use crate::{contract::SourceCodeMetadata, EtherscanError, Result};
+use ethers_core::{abi::Abi, types::Address};
 use semver::Version;
-
-use crate::{EtherscanError, Result};
+use serde::{Deserialize, Deserializer};
 
 static SOLC_BIN_LIST_URL: &str =
     "https://raw.githubusercontent.com/ethereum/solc-bin/gh-pages/bin/list.txt";
 
-/// Given the compiler version  lookup the build metadata
-/// and return full semver
-/// i.e. `0.8.13` -> `0.8.13+commit.abaa5c0e`
+/// Given a Solc [Version], lookup the build metadata and return the full SemVer.
+/// e.g. `0.8.13` -> `0.8.13+commit.abaa5c0e`
 pub async fn lookup_compiler_version(version: &Version) -> Result<Version> {
     let response = reqwest::get(SOLC_BIN_LIST_URL).await?.text().await?;
-    let version = format!("{}", version);
+    // Ignore extra metadata (`pre` or `build`)
+    let version = format!("{}.{}.{}", version.major, version.minor, version.patch);
     let v = response
         .lines()
         .find(|l| !l.contains("nightly") && l.contains(&version))
-        .map(|l| l.trim_start_matches("soljson-v").trim_end_matches(".js").to_owned())
+        .map(|l| l.trim_start_matches("soljson-v").trim_end_matches(".js"))
         .ok_or(EtherscanError::MissingSolcVersion(version))?;
 
     Ok(v.parse().expect("failed to parse semver"))
 }
 
+/// Return None if empty, otherwise parse as [Address].
+pub fn deserialize_address_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> std::result::Result<Option<Address>, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    if s.is_empty() {
+        Ok(None)
+    } else {
+        let addr: Address = s.parse().map_err(serde::de::Error::custom)?;
+        Ok(Some(addr))
+    }
+}
+
+/// Deserializes as JSON:
+///
+/// `{ "SourceCode": "{{ .. }}", ..}`
+///
+/// or
+///
+/// `{ "SourceCode": "..", .. }`
+pub fn deserialize_stringified_source_code<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> std::result::Result<SourceCodeMetadata, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    if s.starts_with("{{") && s.ends_with("}}") {
+        let s = &s[1..s.len() - 1];
+        serde_json::from_str(s).map_err(serde::de::Error::custom)
+    } else {
+        Ok(SourceCodeMetadata::SourceCode(s))
+    }
+}
+
+/// Deserializes as JSON: "\[...\]"
+pub fn deserialize_stringified_abi<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> std::result::Result<Abi, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    serde_json::from_str(&s).map_err(serde::de::Error::custom)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::tests::run_at_least_duration;
+    use crate::{contract::SourceCodeLanguage, tests::run_at_least_duration};
     use semver::{BuildMetadata, Prerelease};
     use serial_test::serial;
     use std::time::Duration;
@@ -52,5 +93,63 @@ mod tests {
             assert!(matches!(err, EtherscanError::MissingSolcVersion(_)));
         })
         .await
+    }
+
+    #[test]
+    fn can_deserialize_address_opt() {
+        #[derive(Deserialize)]
+        struct Test {
+            #[serde(deserialize_with = "deserialize_address_opt")]
+            address: Option<Address>,
+        }
+
+        // https://api.etherscan.io/api?module=contract&action=getsourcecode&address=0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413
+        let json = r#"{"address":""}"#;
+        let de: Test = serde_json::from_str(json).unwrap();
+        assert_eq!(de.address, None);
+
+        // https://api.etherscan.io/api?module=contract&action=getsourcecode&address=0xDef1C0ded9bec7F1a1670819833240f027b25EfF
+        let json = r#"{"address":"0x4af649ffde640ceb34b1afaba3e0bb8e9698cb01"}"#;
+        let de: Test = serde_json::from_str(json).unwrap();
+        let expected = "0x4af649ffde640ceb34b1afaba3e0bb8e9698cb01".parse().unwrap();
+        assert_eq!(de.address, Some(expected));
+    }
+
+    #[test]
+    fn can_deserialize_stringified_abi() {
+        #[derive(Deserialize)]
+        struct Test {
+            #[serde(deserialize_with = "deserialize_stringified_abi")]
+            abi: Abi,
+        }
+
+        let json = r#"{"abi": "[]"}"#;
+        let de: Test = serde_json::from_str(json).unwrap();
+        assert_eq!(de.abi, Abi::default());
+    }
+
+    #[test]
+    fn can_deserialize_stringified_source_code() {
+        #[derive(Deserialize)]
+        struct Test {
+            #[serde(deserialize_with = "deserialize_stringified_source_code")]
+            source_code: SourceCodeMetadata,
+        }
+
+        let src = "source code text";
+
+        let json = r#"{
+            "source_code": "{{ \"language\": \"Solidity\", \"sources\": {\"Contract\": { \"content\": \"source code text\" } } }}"
+        }"#;
+        let de: Test = serde_json::from_str(json).unwrap();
+        assert!(matches!(de.source_code.language().unwrap(), SourceCodeLanguage::Solidity));
+        assert_eq!(de.source_code.sources().len(), 1);
+        assert_eq!(de.source_code.sources().get("Contract").unwrap().content, src);
+        #[cfg(feature = "ethers-solc")]
+        assert!(matches!(de.source_code.settings().unwrap(), None));
+
+        let json = r#"{"source_code": "source code text"}"#;
+        let de: Test = serde_json::from_str(json).unwrap();
+        assert_eq!(de.source_code.source_code(), src);
     }
 }
