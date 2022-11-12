9162e71bdfcd63e6b38d5899635def0857a9b460
Noah Citron <noah@jeff.org>
2022-11-12 17:55:29 -0500

    remove comment

--- ethers-providers/src/transports/retry.rs
+++ ethers-providers/src/transports/retry.rs
@@ -289,12 +289,7 @@ where
                 // try to extract the requested backoff from the error or compute the next backoff
                 // based on retry count
                 let mut next_backoff = self.policy.backoff_hint(&err).unwrap_or_else(|| {
-                    // using `retry_number` for creating back pressure because
-                    // of already queued requests
-                    // this increases exponentially with retries and adds a delay based on how many
-                    // requests are currently queued
                     Duration::from_millis(
-                        // self.initial_backoff.as_millis().pow(rate_limit_retry_number) as u64,
                         self.initial_backoff.as_millis() as u64
                     )
                 });
