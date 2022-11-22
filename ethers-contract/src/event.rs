abd0827204a98e81b825ce15af5ef82fd170bdf2
Andrea Simeoni <>
2022-11-22 10:35:32 +0100

    Function to set Event address

--- ethers-contract/src/event.rs
+++ ethers-contract/src/event.rs
@@ -3,7 +3,7 @@
 use crate::{log::LogMeta, stream::EventStream, ContractError, EthLogDecode};
 use ethers_core::{
     abi::{Detokenize, RawLog},
-    types::{BlockNumber, Filter, Log, Topic, H256},
+    types::{BlockNumber, Filter, Log, Topic, H256, ValueOrArray, H160},
 };
 use ethers_providers::{FilterWatcher, Middleware, PubsubClient, SubscriptionStream};
 use std::{borrow::Cow, marker::PhantomData};
@@ -106,6 +106,12 @@ impl<M, D: EthLogDecode> Event<'_, M, D> {
     /// Sets the filter's 3rd topic
     pub fn topic3<T: Into<Topic>>(mut self, topic: T) -> Self {
         self.filter.topics[3] = Some(topic.into());
+        self
+    }
+
+    /// Sets the filter's address.
+    pub fn address(mut self, address: ValueOrArray<H160>) -> Self {
+        self.filter.address = Some(address);
         self
     }
 }
