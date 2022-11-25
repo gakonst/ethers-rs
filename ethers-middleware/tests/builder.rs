e089e7ce1bdb14ccb9b6397326606ddbac93ef34
Andrea Simeoni <>
2022-11-25 09:06:21 +0100

    Unit tests

--- ethers-middleware/tests/builder.rs
+++ ethers-middleware/tests/builder.rs
@@ -13,7 +13,7 @@ mod tests {
     use ethers_signers::{LocalWallet, Signer};
 
     #[tokio::test]
-    async fn build_middleware_stack() {
+    async fn build_raw_middleware_stack() {
         let (provider, mock) = Provider::mocked();
 
         let signer = LocalWallet::new(&mut thread_rng());
@@ -25,6 +25,35 @@ mod tests {
             .wrap_into(|p| GasOracleMiddleware::new(p, EthGasStation::new(None)))
             .wrap_into(|p| SignerMiddleware::new(p, signer))
             .wrap_into(|p| NonceManagerMiddleware::new(p, address))
+            .build();
+
+        // push a response
+        mock.push(U64::from(12u64)).unwrap();
+        let block: U64 = provider.get_block_number().await.unwrap();
+        assert_eq!(block.as_u64(), 12);
+
+        provider.get_block_number().await.unwrap_err();
+
+        // 2 calls were made
+        mock.assert_request("eth_blockNumber", ()).unwrap();
+        mock.assert_request("eth_blockNumber", ()).unwrap();
+        mock.assert_request("eth_blockNumber", ()).unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn build_declarative_middleware_stack() {
+        let (provider, mock) = Provider::mocked();
+
+        let signer = LocalWallet::new(&mut thread_rng());
+        let address = signer.address();
+        let escalator = GeometricGasPrice::new(1.125, 60u64, None::<u64>);
+        let gas_oracle = EthGasStation::new(None);
+
+        let provider = ProviderBuilder::from(provider)
+            .wrap_into(|p| GasEscalatorMiddleware::new(p, escalator, Frequency::PerBlock))
+            .gas_oracle(gas_oracle)
+            .with_signer(signer)
+            .nonce_manager(address)
             .build();
 
         // push a response
