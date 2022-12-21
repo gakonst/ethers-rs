2a2ba3ee3cdf08d7f6a554c7e927e02bee8a647c
Andrea Simeoni <>
2022-12-21 23:11:04 +0100

    ci: - Run/Build  examples in a single step to avoid duplicated scripts - Removed ci.yaml step "Build all examples"

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,25 +1,42 @@
-set -e
+#!/bin/bash
+
+set -e; # Fail fast
 # shellcheck shell=bash
 
 # examples that we can't run because they require some additional infra, docker or ledger for example
 ignored=(
-  "moonbeam_with_abi"
-  "ipc"
-  "ledger"
-  "paginated_logs"
-  "subscribe_logs"
-  "trezor"
-  "yubi"
-  "remove_liquidity"
+  "examples-contracts:contract_deploy_moonbeam"
+  "examples-providers:ipc"
+  "examples-wallets:ledger"
+  "examples-wallets:trezor"
+  "examples-wallets:yubi"
+  "examples-transactions:txn_remove_liquidity"
 )
 
-# run all examples
-for file in examples/*.rs; do
-  name="$(echo "$file" | cut -f 1 -d '.')"
-  if [[ "${ignored[*]}" =~ $(basename "$name") ]]; then
-    echo "skipping: $file"
-    continue
-  fi
-  echo "running: $file"
-  cargo r -p ethers --example "$(basename "$name")" --features "ethers-solc rustls ws"
+example_crates=$(cargo metadata --format-version 1 | 
+                 jq -c '.workspace_members' | 
+                 jq -r 'map(select(startswith("examples")) |
+                 sub("\\s.*$";"")) | .[]')
+
+for crate in $example_crates; do 
+    # Remove "examples-" prefix from crate name (e.g. examples-contracts => contracts)
+    cratedir="${crate#examples-}"
+    srcdir="examples/$cratedir/examples"
+    # Retrieve all example files in crate:
+    # Transform the absolute path into the filename (e.g. some-path/deploy_anvil.rs => deploy_anvil)
+    example_files=$(find $srcdir -type f -name '*.rs' -exec basename {} \; | sed 's/\.[^.]*$//')
+
+    for file in $example_files; do
+        # Build the example
+        echo "Building example: $crate:$file"
+        cargo build -p $crate --example $file
+
+        # Run example
+        if [[ "${ignored[*]}" =~ $(basename "$crate:$file") ]]; then
+            echo "skipping: $crate:$file"
+            continue
+        fi
+        echo "running $crate:$file"
+        cargo run -p $crate --example $file
+    done
 done
