2975c976cad21ab51551827620c578a8b91dd589
Andrea Simeoni <>
2022-12-27 19:25:53 +0100

    review: Removed prefixes from file names

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -5,12 +5,12 @@ set -e; # Fail fast
 
 # examples that we can't run because they require some additional infra, docker or ledger for example
 ignored=(
-  "examples-contracts:contract_deploy_moonbeam"
+  "examples-contracts:deploy_moonbeam"
   "examples-providers:ipc"
   "examples-wallets:ledger"
   "examples-wallets:trezor"
   "examples-wallets:yubi"
-  "examples-transactions:txn_remove_liquidity"
+  "examples-transactions:remove_liquidity"
 )
 
 example_crates=$(cargo metadata --format-version 1 | 
@@ -31,7 +31,7 @@ for crate in $example_crates; do
         echo "Building example: $crate:$file"
         cargo build -p $crate --example $file
 
-        # Run example
+        # Run the example
         if [[ "${ignored[*]}" =~ $(basename "$crate:$file") ]]; then
             echo "skipping: $crate:$file"
             continue
