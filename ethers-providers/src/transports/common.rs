ddb456ea6a1ecb4c17fe63852136ef90d2d2e392
oblique <psyberbits@gmail.com>
2023-01-10 19:55:40 +0200

    fix: Replace deprecated base64::encode with BASE64_STANDARD

--- ethers-providers/src/transports/common.rs
+++ ethers-providers/src/transports/common.rs
@@ -1,5 +1,6 @@
 // Code adapted from: https://github.com/althea-net/guac_rs/tree/master/web3/src/jsonrpc
 
+use base64::prelude::*;
 use ethers_core::types::U256;
 use serde::{
     de::{self, MapAccess, Unexpected, Visitor},
@@ -191,7 +192,7 @@ pub enum Authorization {
 
 impl Authorization {
     pub fn basic(username: impl Into<String>, password: impl Into<String>) -> Self {
-        let auth_secret = base64::encode(username.into() + ":" + &password.into());
+        let auth_secret = BASE64_STANDARD.encode(username.into() + ":" + &password.into());
         Self::Basic(auth_secret)
     }
 
