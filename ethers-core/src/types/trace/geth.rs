31350e5bb806f484d1b7bd0f219f91533b46fa52
wangyufei <wangyufei.kutu@bytedance.com>
2023-02-06 13:53:44 +0800

    feat: add prestateTracer

--- ethers-core/src/types/trace/geth.rs
+++ ethers-core/src/types/trace/geth.rs
@@ -44,6 +44,22 @@ pub struct StructLog {
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct FourByteFrame(pub BTreeMap<String, u64>);
 
+// https://github.com/ethereum/go-ethereum/blob/a9ef135e2dd53682d106c6a2aede9187026cc1de/eth/tracers/native/prestate.go#L36
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PreStateFrame(pub BTreeMap<Address, AccountState>);
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AccountState {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub balance: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub code: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub nonce: Option<u64>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub storage: Option<BTreeMap<H256, H256>>,
+}
+
 // https://github.com/ethereum/go-ethereum/blob/a9ef135e2dd53682d106c6a2aede9187026cc1de/eth/tracers/native/call.go#L37
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CallFrame {
@@ -73,6 +89,7 @@ pub enum GethTraceFrame {
     Default(DefaultFrame),
     FourByteTracer(FourByteFrame),
     CallTracer(CallFrame),
+    PreStateTracer(PreStateFrame),
 }
 
 impl From<DefaultFrame> for GethTraceFrame {
@@ -90,6 +107,12 @@ impl From<FourByteFrame> for GethTraceFrame {
 impl From<CallFrame> for GethTraceFrame {
     fn from(value: CallFrame) -> Self {
         GethTraceFrame::CallTracer(value)
+    }
+}
+
+impl From<PreStateFrame> for GethTraceFrame {
+    fn from(value: PreStateFrame) -> Self {
+        GethTraceFrame::PreStateTracer(value)
     }
 }
 
@@ -121,6 +144,8 @@ pub enum GethDebugBuiltInTracerType {
     FourByteTracer,
     #[serde(rename = "callTracer")]
     CallTracer,
+    #[serde(rename = "prestateTracer")]
+    PreStateTracer,
 }
 
 /// Available tracers
