4a226ccbb58a284261d7d013bd180644aef6e9c5
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-02-14 00:50:16 +0100

    fmt Codec derive

--- ethers-contract/ethers-contract-derive/src/codec.rs
+++ ethers-contract/ethers-contract-derive/src/codec.rs
@@ -1,32 +1,32 @@
 //! Helper functions for deriving `EthAbiType`
 
 use ethers_core::macros::ethers_core_crate;
-
 use quote::quote;
 use syn::DeriveInput;
 
 /// Generates the `AbiEncode` + `AbiDecode` implementation
 pub fn derive_codec_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
     let name = &input.ident;
-    let core_crate = ethers_core_crate();
+    let ethers_core = ethers_core_crate();
 
     quote! {
-        impl  #core_crate::abi::AbiDecode for #name {
-            fn decode(bytes: impl AsRef<[u8]>) -> ::std::result::Result<Self, #core_crate::abi::AbiError> {
-                if let #core_crate::abi::ParamType::Tuple(params) = <Self as #core_crate::abi::AbiType>::param_type() {
-                  let tokens = #core_crate::abi::decode(&params, bytes.as_ref())?;
-                  Ok(<Self as #core_crate::abi::Tokenizable>::from_token(#core_crate::abi::Token::Tuple(tokens))?)
+        impl #ethers_core::abi::AbiDecode for #name {
+            fn decode(bytes: impl AsRef<[u8]>) -> ::std::result::Result<Self, #ethers_core::abi::AbiError> {
+                if let #ethers_core::abi::ParamType::Tuple(params) = <Self as #ethers_core::abi::AbiType>::param_type() {
+                    let tokens = #ethers_core::abi::decode(&params, bytes.as_ref())?;
+                    Ok(<Self as #ethers_core::abi::Tokenizable>::from_token(#ethers_core::abi::Token::Tuple(tokens))?)
                 } else {
                     Err(
-                        #core_crate::abi::InvalidOutputType("Expected tuple".to_string()).into()
+                        #ethers_core::abi::InvalidOutputType("Expected tuple".to_string()).into()
                     )
                 }
             }
         }
-        impl #core_crate::abi::AbiEncode for #name {
+
+        impl #ethers_core::abi::AbiEncode for #name {
             fn encode(self) -> ::std::vec::Vec<u8> {
-                let tokens =  #core_crate::abi::Tokenize::into_tokens(self);
-                #core_crate::abi::encode(&tokens)
+                let tokens = #ethers_core::abi::Tokenize::into_tokens(self);
+                #ethers_core::abi::encode(&tokens)
             }
         }
     }
