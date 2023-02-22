1485a7822217bdf2faf1fd07c395f5e01b35766b
James <james@prestwi.ch>
2023-02-21 16:36:28 -0800

    chore: clippy

--- ethers-contract/tests/it/contract_call.rs
+++ ethers-contract/tests/it/contract_call.rs
@@ -1,14 +1,12 @@
-use ethers_contract::ContractFactory;
 use ethers_contract_derive::abigen;
-use ethers_core::{abi::Address, utils::Anvil};
-use ethers_providers::{MiddlewareError, Provider};
+use ethers_core::abi::Address;
+use ethers_providers::{Provider};
 use std::{
-    convert::TryFrom,
     future::{Future, IntoFuture},
     sync::Arc,
 };
 
-use crate::common::compile_contract;
+
 
 #[tokio::test]
 async fn contract_call_into_future_is_send() {
