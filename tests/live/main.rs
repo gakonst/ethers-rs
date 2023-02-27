cdd9d84674402a3d7f2f66094aa25e99a3dfccd8
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-02-27 21:45:16 +0100

    ci: run live tests separately

--- tests/live/main.rs
+++ tests/live/main.rs
@@ -1,3 +1,8 @@
+//! Ethers live tests.
+//!
+//! If a feature or external binary is added, like Solc, please also update
+//! `.github/workflows/ci.yml` at `job.live-test`.
+
 #![cfg(not(target_arch = "wasm32"))]
 
 #[cfg(feature = "celo")]
