6326108fd2e50c80cf3a6f8e8a035790a4a54c37
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-02-28 01:16:23 +0100

    fix: subscribe instead of watch

--- examples/subscriptions/examples/subscribe_blocks.rs
+++ examples/subscriptions/examples/subscribe_blocks.rs
@@ -1,12 +1,11 @@
 use ethers::providers::{Middleware, Provider, StreamExt, Ws};
 use eyre::Result;
-use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let ws_endpoint = "wss://mainnet.infura.io/ws/v3/c60b0bb42f8a4c6481ecd229eddaca27";
-    let ws = Ws::connect(ws_endpoint).await?;
-    let provider = Provider::new(ws).interval(Duration::from_millis(2000));
+    let provider =
+        Provider::<Ws>::connect("wss://mainnet.infura.io/ws/v3/c60b0bb42f8a4c6481ecd229eddaca27")
+            .await?;
     let mut stream = provider.subscribe_blocks().await?.take(1);
     while let Some(block) = stream.next().await {
         println!(
