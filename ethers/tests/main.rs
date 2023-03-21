3286469b38fd5dce5e002bd5cef5e12d6b1c3172
Georgios Konstantopoulos <me@gakonst.com>
2023-03-20 22:12:45 -0700

    refactor: move ethers/live up a dir

--- ethers/tests/main.rs
+++ ethers/tests/main.rs
@@ -1,8 +1,4 @@
-//! Ethers live tests.
-//!
-//! If a feature or external binary is added, like Solc, please also update
-//! `.github/workflows/ci.yml` at `job.live-test`.
-
+//! Ethers integration tests.
 #![cfg(not(target_arch = "wasm32"))]
 
 #[cfg(feature = "celo")]
