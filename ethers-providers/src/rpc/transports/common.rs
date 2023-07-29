be2dfaedaacf3a2cf5b99f6365ce578da9a77cac
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-07-28 20:01:32 -0400

    make doc automatic link

--- ethers-providers/src/rpc/transports/common.rs
+++ ethers-providers/src/rpc/transports/common.rs
@@ -355,7 +355,7 @@ impl JwtAuth {
     }
 }
 
-/// Claims struct as defined in https://github.com/ethereum/execution-apis/blob/main/src/engine/authentication.md#jwt-claims
+/// Claims struct as defined in <https://github.com/ethereum/execution-apis/blob/main/src/engine/authentication.md#jwt-claims>
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Claims {
     /// issued-at claim. Represented as seconds passed since UNIX_EPOCH.
