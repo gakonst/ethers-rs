4ac0058f50571954596cabbe13a3d57351d9adac
JP <36560907+0xfourzerofour@users.noreply.github.com>
2023-09-16 05:33:48 -0400

    fix(oracle): update helper function to use correct converion maths (#2602)

--- ethers-middleware/src/gas_oracle/mod.rs
+++ ethers-middleware/src/gas_oracle/mod.rs
@@ -152,8 +152,8 @@ pub trait GasOracle: Send + Sync + Debug {
 
 #[inline]
 #[doc(hidden)]
-pub(crate) fn from_gwei_f64(gwei: f64) -> U256 {
-    ethers_core::types::u256_from_f64_saturating(gwei) * GWEI_TO_WEI_U256
+pub fn from_gwei_f64(gwei: f64) -> U256 {
+    U256::from((gwei * GWEI_TO_WEI as f64).ceil() as u64)
 }
 
 #[cfg(test)]
@@ -165,5 +165,13 @@ mod tests {
         let as_u256: U256 = GWEI_TO_WEI.into();
         assert_eq!(as_u256, GWEI_TO_WEI_U256);
         assert_eq!(GWEI_TO_WEI_U256.as_u64(), GWEI_TO_WEI);
+    }
+
+    #[test]
+    fn test_gwei_conversion() {
+        let max_priority_fee: f64 = 1.8963421368;
+
+        let result = from_gwei_f64(max_priority_fee);
+        assert_eq!(result, U256::from(1896342137));
     }
 }
