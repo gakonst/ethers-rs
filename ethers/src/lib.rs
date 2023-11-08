3eedcefbda39e5a1d7e752677ef3b152a34687ab
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-08 20:14:22 +0100

    fmt

--- ethers/src/lib.rs
+++ ethers/src/lib.rs
@@ -1,7 +1,7 @@
 //! # ethers-rs
 //!
 //! A complete Ethereum and Celo Rust library.
-//! 
+//!
 //! <div class="warning">
 //! This crate is in the process of being deprecated.
 //! See <a href="https://github.com/gakonst/ethers-rs/issues/2667">#2667</a> for more information.
