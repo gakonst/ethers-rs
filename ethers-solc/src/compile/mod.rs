f8485a58d7c0708c766bf07494033fb1d936d631
Kristofer Peterson <svenski123@users.noreply.github.com>
2023-11-12 15:17:02 +0100

    chore: bump solc

--- ethers-solc/src/compile/mod.rs
+++ ethers-solc/src/compile/mod.rs
@@ -874,7 +874,7 @@ mod tests {
             (">=0.4.0 <0.5.0", "0.4.26"),
             // latest - this has to be updated every time a new version is released.
             // Requires the SVM version list to be updated as well.
-            (">=0.5.0", "0.8.22"),
+            (">=0.5.0", "0.8.23"),
         ] {
             let source = source(pragma);
             let res = Solc::detect_version(&source).unwrap();
