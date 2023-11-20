2d2599f90237376e443163e3f05b887c6141dc78
anikaraghu <anika.raghuvanshi@coinbase.com>
2023-11-20 15:28:37 -0800

    Rename field camelCase

--- ethers-core/src/types/transaction/optimism.rs
+++ ethers-core/src/types/transaction/optimism.rs
@@ -30,6 +30,7 @@ pub struct DepositTransaction {
 
     /// If true, the transaction does not interact with the L2 block gas pool.
     /// Note: boolean is disabled (enforced to be false) starting from the Regolith upgrade.
+    #[serde(rename = "isSystemTx")]
     pub is_system_tx: bool,
 }
 
