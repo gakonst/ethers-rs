73735316d7b3cb099a5f0dade3f85d57baaf8754
Yorke Rhodes <email@yorke.dev>
2023-11-22 23:22:11 -0500

    Fix build

--- ethers-middleware/src/multicall.rs
+++ ethers-middleware/src/multicall.rs
@@ -1,9 +1,11 @@
 use std::{cmp::Ordering, sync::Arc, time::Instant};
 
 use async_trait::async_trait;
-use ethers_contract::{multicall::Multicall, BaseContract, ContractCall, MulticallError};
+use ethers_contract::{
+    multicall::Multicall, BaseContract, ContractCall, ContractError, MulticallError,
+};
 use ethers_core::{
-    abi::{Bytes, Token, Tokenizable},
+    abi::{encode, Token, Tokenizable},
     types::{transaction::eip2718::TypedTransaction, BlockId},
 };
 use ethers_providers::{Middleware, MiddlewareError};
@@ -13,117 +15,17 @@ use thiserror::Error;
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
 
-type MulticallResult = Result<Token, Bytes>;
-type MulticallRequest<M: Middleware> = (ContractCall<M, Bytes>, oneshot::Sender<MulticallResult>);
+type MulticallResult<M> = Result<Token, MulticallError<M>>;
+type MulticallRequest<M> = (ContractCall<M, Token>, oneshot::Sender<MulticallResult<M>>);
 
 #[derive(Debug)]
 /// Middleware used for transparently leveraging multicall functionality
 pub struct MulticallMiddleware<M: Middleware> {
     inner: Arc<M>,
     contracts: Vec<BaseContract>,
-    multicall: Multicall<M>,
-    callbacks: Vec<oneshot::Sender<MulticallResult>>,
     rx: mpsc::UnboundedReceiver<MulticallRequest<M>>,
     tx: mpsc::UnboundedSender<MulticallRequest<M>>,
-    checkpoint: instant::Instant,
     frequency: Duration,
-}
-
-impl<M> MulticallMiddleware<M>
-where
-    M: Middleware,
-{
-    /// Instantiates the nonce manager with a 0 nonce. The `address` should be the
-    /// address which you'll be sending transactions from
-    /// TODO: support multiple contract ABIs // 4byte DB
-    pub async fn new(
-        inner: M,
-        contracts: Vec<BaseContract>,
-        batch_frequency: Duration,
-    ) -> Result<Self, MulticallError<M>> {
-        // TODO: support custom multicall address
-        let multicall = Multicall::new(inner, None).await?;
-        let callbacks = Vec::new();
-
-        let (tx, rx) = mpsc::unbounded_channel();
-
-        let timestamp = Instant::now();
-
-        Ok(Self {
-            inner,
-            multicall,
-            callbacks,
-            contracts,
-            tx,
-            rx,
-            checkpoint: timestamp,
-            frequency: batch_frequency,
-        })
-    }
-
-    pub async fn run(&mut self) -> Result<(), MulticallMiddlewareError<M>> {
-        loop {
-            let maybe_request = self.rx.try_recv();
-            match maybe_request {
-                Ok((call, callback)) => {
-                    self.multicall.add_call(call, true);
-                    self.callbacks.push(callback);
-                    // keep filling batch until channel is empty (or closed)
-                    continue;
-                }
-                Err(mpsc::error::TryRecvError::Disconnected) => {
-                    // TODO: exit?
-                }
-                Err(mpsc::error::TryRecvError::Empty) => {
-                    // TODO: consider sleeping here?
-                }
-            }
-
-            // check if batch is non-empty and frequency has elapsed since last batch was sent
-            if self.callbacks.len() > 0
-                && self.checkpoint.elapsed().cmp(&self.frequency) == Ordering::Greater
-            {
-                let maybe_results = self.multicall.call_raw().await;
-                match maybe_results {
-                    Ok(results) => {
-                        self.multicall.clear_calls();
-
-                        for (result, callback) in results.into_iter().zip(self.callbacks.drain(..))
-                        {
-                            callback.send(result);
-                        }
-
-                        self.checkpoint = Instant::now();
-                    }
-                    Err(MulticallError::ContractError(ce)) => {
-                        // TODO: bubble up to callback?
-                    }
-                    Err(MulticallError::InvalidChainId(id)) => {
-                        // TODO: exit?
-                    }
-                    Err(MulticallError::IllegalRevert) => {
-                        // TODO: idk
-                    }
-                }
-            }
-        }
-    }
-
-    fn call_from_tx<D: Tokenizable>(
-        &self,
-        tx: &TypedTransaction,
-        block: Option<BlockId>,
-    ) -> Option<ContractCall<M, D>> {
-        if let Some(data) = tx.data() {
-            for contract in self.contracts.iter() {
-                if let Ok(function) = contract.get_fn_from_input(data) {
-                    return Some(ContractCall::new(*tx, *function, self.inner, block));
-                }
-            }
-        }
-
-        None
-    }
 }
 
 #[derive(Error, Debug)]
@@ -132,6 +34,9 @@ pub enum MulticallMiddlewareError<M: Middleware> {
     /// Thrown when the internal middleware errors
     #[error("{0}")]
     MiddlewareError(M::Error),
+    /// Thrown when the internal multicall errors
+    #[error("{0}")]
+    MulticallError(MulticallError<M>),
 }
 
 impl<M: Middleware> MiddlewareError for MulticallMiddlewareError<M> {
@@ -144,7 +49,94 @@ impl<M: Middleware> MiddlewareError for MulticallMiddlewareError<M> {
     fn as_inner(&self) -> Option<&Self::Inner> {
         match self {
             MulticallMiddlewareError::MiddlewareError(e) => Some(e),
+            MulticallMiddlewareError::MulticallError(e) => e.as_middleware_error(),
         }
+    }
+}
+
+impl<M: Middleware> From<MulticallError<M>> for MulticallMiddlewareError<M> {
+    fn from(value: MulticallError<M>) -> Self {
+        MulticallMiddlewareError::MulticallError(value)
+    }
+}
+
+impl<M> MulticallMiddleware<M>
+where
+    M: Middleware,
+{
+    /// Instantiates the multicall middleware to recognize the given `contracts` selectors
+    /// and batch calls in a single inner call every `frequency` interval
+    pub fn new(
+        inner: M,
+        contracts: Vec<BaseContract>,
+        frequency: Duration,
+    ) -> Result<Self, MulticallError<M>> {
+        let (tx, rx) = mpsc::unbounded_channel();
+
+        Ok(Self { inner: Arc::new(inner), contracts, tx, rx, frequency })
+    }
+
+    pub async fn run(&mut self) -> Result<(), MulticallMiddlewareError<M>> {
+        // TODO: support custom multicall address
+        let mut multicall = Multicall::new(self.inner.clone(), None).await?;
+        let mut callbacks = Vec::new();
+        let mut checkpoint = Instant::now();
+
+        loop {
+            let maybe_request = self.rx.try_recv();
+            match maybe_request {
+                Ok((call, callback)) => {
+                    multicall.add_call(call, true);
+                    callbacks.push(callback);
+                    // keep filling batch until channel is empty (or disconnected)
+                    continue;
+                }
+                Err(mpsc::error::TryRecvError::Disconnected) => {
+                    panic!("multicall channel disconnected");
+                }
+                Err(mpsc::error::TryRecvError::Empty) => {
+                    // TODO: consider sleeping here
+                }
+            }
+
+            // check if batch is non-empty and frequency has elapsed since last batch was sent
+            if callbacks.len() > 0 && checkpoint.elapsed().cmp(&self.frequency) == Ordering::Greater
+            {
+                let results = multicall.call_raw().await?;
+                multicall.clear_calls();
+
+                for (result, callback) in results.into_iter().zip(callbacks.drain(..)) {
+                    let response =
+                        result.map_err(|e| MulticallError::ContractError(ContractError::Revert(e)));
+                    if let Err(e) = callback.send(response) {
+                        panic!("oneshot channel closed: {:?}", e);
+                    }
+                }
+
+                checkpoint = Instant::now();
+            }
+        }
+    }
+
+    fn call_from_tx<D: Tokenizable>(
+        &self,
+        tx: &TypedTransaction,
+        block: Option<BlockId>,
+    ) -> Option<ContractCall<M, D>> {
+        if let Some(data) = tx.data() {
+            for contract in self.contracts.iter() {
+                if let Ok(function) = contract.get_fn_from_input(data) {
+                    return Some(ContractCall::new(
+                        tx.clone(),
+                        function.clone(),
+                        self.inner.clone(),
+                        block,
+                    ));
+                }
+            }
+        }
+
+        None
     }
 }
 
@@ -166,15 +158,22 @@ where
         &self,
         tx: &TypedTransaction,
         block: Option<BlockId>,
-    ) -> Result<Bytes, Self::Error> {
+    ) -> Result<ethers_core::types::Bytes, Self::Error> {
         if let Some(call) = self.call_from_tx(tx, block) {
             let (tx, rx) = oneshot::channel();
 
-            self.tx.send((call, tx))?;
+            if let Err(e) = self.tx.send((call, tx)) {
+                panic!("multicall channel disconnected: {:?}", e);
+            };
 
-            return rx.await;
+            match rx.await {
+                Err(e) => panic!("multicall channel disconnected: {:?}", e),
+                Ok(response) => {
+                    return response.map(|token| encode(&[token]).into()).map_err(|e| e.into());
+                }
+            }
         }
 
-        return self.inner.call(tx, block).await;
+        return self.inner.call(tx, block).await.map_err(MulticallMiddlewareError::from_err);
     }
 }
