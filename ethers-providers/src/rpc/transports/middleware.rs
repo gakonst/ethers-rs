e89d5fa6ba0ab8635c72b69e31b913697ef89dff
dharjeezy <dharjeezy@gmail.com>
2024-04-11 11:00:41 +0100

    match json rpc error and subtle test

--- ethers-providers/src/rpc/transports/middleware.rs
+++ ethers-providers/src/rpc/transports/middleware.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
 
+use crate::{
+    rpc::transports::http::{ClientError, Provider},
+    HttpClientError::ReqwestError,
+    JsonRpcClient, ProviderError,
+};
 use anyhow::anyhow;
-use reqwest::{Response, StatusCode, Url};
-use reqwest_chain::Chainer;
-use reqwest_middleware::Error;
-use ethers_core::types::transaction::request;
-use crate::HttpClientError::ReqwestError;
-use crate::ProviderError;
-use crate::rpc::transports::http::{ClientError, Provider};
+use ethers_core::types::{transaction::request, Block, H256};
+use reqwest::{Client, Response, StatusCode, Url};
+use reqwest_chain::{ChainMiddleware, Chainer};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, Error};
 
 /// Middleware for switching between providers on failures
 pub struct SwitchProviderMiddleware {
@@ -37,20 +39,27 @@ impl Chainer for SwitchProviderMiddleware {
         _state: &mut Self::State,
         request: &mut reqwest::Request,
     ) -> Result<Option<reqwest::Response>, Error> {
+        println!("in chain");
         let mut next_state = |client_error: Option<ClientError>| {
             let active_index = _state.active_provider_index;
             _state.prev_stat.insert(active_index, client_error);
             let mut next_index = _state.active_provider_index + 1;
             if next_index >= self.providers.len() {
-                let res = _state.prev_stat.iter().filter_map(|(_, error_option)| {
-                    error_option.as_ref().and_then(|error| match error {
-                        ReqwestError(err) if err.status() == Some(StatusCode::NOT_FOUND) => Some(()),
-                        _ => None
+                let res = _state
+                    .prev_stat
+                    .iter()
+                    .filter_map(|(_, error_option)| {
+                        error_option.as_ref().and_then(|error| match error {
+                            ReqwestError(err) if err.status() == Some(StatusCode::NOT_FOUND) => {
+                                Some(())
+                            }
+                            _ => None,
+                        })
                     })
-                }).any(|_| true);
+                    .any(|_| true);
 
                 if res {
-                    return Err(anyhow!("All providers returned {:?}", StatusCode::NOT_FOUND))?
+                    return Err(anyhow!("All providers returned {:?}", StatusCode::NOT_FOUND))?;
                 }
                 next_index = 0;
             }
@@ -66,24 +75,27 @@ impl Chainer for SwitchProviderMiddleware {
         match result {
             Ok(response) => {
                 let body = response.bytes().await?;
+                println!("in body {:?}", &body);
 
-                let raw = match serde_json::from_slice(&body) {
+                match serde_json::from_slice(&body) {
                     Ok(crate::rpc::common::Response::Success { result, .. }) => {
-                        return Ok(None)
-                    },
+                        //return Ok(Some(response));
+                        println!("got result {:?}", &result);
+                    }
                     Ok(crate::rpc::common::Response::Error { error, .. }) => {
                         let _ = next_state(Some(ClientError::JsonRpcError(error)))?;
-                    },
+                    }
                     Ok(_) => {
                         let err = ClientError::SerdeJson {
-                            err: serde::de::Error::custom("unexpected notification over HTTP transport"),
+                            err: serde::de::Error::custom(
+                                "unexpected notification over HTTP transport",
+                            ),
                             text: String::from_utf8_lossy(&body).to_string(),
                         };
                         let _ = next_state(Some(err))?;
-                        //return Err(err)
                     }
                     Err(err) => {
-                       let error =   ClientError::SerdeJson {
+                        let error = ClientError::SerdeJson {
                             err,
                             text: String::from_utf8_lossy(&body).to_string(),
                         };
@@ -91,12 +103,11 @@ impl Chainer for SwitchProviderMiddleware {
                         let _ = next_state(Some(error))?;
                     }
                 };
-
-            },
+            }
             Err(e) => {
                 log::trace!(target:"ethers-providers", "Possibly encountered an os error submitting request, switching provider {e:?}");
                 let _ = next_state(None)?;
-            },
+            }
         }
 
         Ok(None)
@@ -104,5 +115,40 @@ impl Chainer for SwitchProviderMiddleware {
 
     fn max_chain_length(&self) -> u32 {
         u32::MAX
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::rpc::{
+        common::Request,
+        transports::{http::Provider, middleware::SwitchProviderMiddleware},
+    };
+    use reqwest::{Client, Url};
+    use reqwest_chain::ChainMiddleware;
+    use reqwest_middleware::ClientBuilder;
+
+    #[tokio::test]
+    async fn test_switch_provider_middleware() {
+        let providers = vec![
+            Provider::new(Url::parse("http://localhost:3510").unwrap()),
+            Provider::new(Url::parse("http://localhost:3500").unwrap()),
+            Provider::new(Url::parse("https://eth.llamarpc.com").unwrap()),
+        ];
+
+        let client = ClientBuilder::new(Client::new())
+            .with(ChainMiddleware::new(SwitchProviderMiddleware::_new(providers.clone())))
+            .build();
+
+        // Send a JSON-RPC request for the latest block
+        let block_num = "latest".to_string();
+        let txn_details = false;
+        let params = (block_num, txn_details);
+
+        let payload = Request::new(0, "eth_getBlockByNumber", params);
+
+        let res = client.post("http://localhost:3510").json(&payload).send().await.unwrap();
+
+        println!("{res:?}");
     }
 }
